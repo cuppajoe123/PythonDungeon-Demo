e7b33ad9278452a7c718b006fbde51b2a2c9ff17
le Monke <joeks05@icloud.com>
2021-07-23 13:22:52 -0500

    updated tiles.py to conform to PEP8

--- tiles.py
+++ tiles.py
@@ -2,12 +2,15 @@ import random
 import items, enemies, actions, world, config
 from player import Player
 
+
 class MapTile:
     """Parent tile class"""
     config.init()
+
     def __init__(self, x, y, ):
         self.x = x
         self.y = y
+
     def intro_text(self):
         raise NotImplementedError()
 
@@ -26,7 +29,7 @@ class MapTile:
         if world.tile_exists(self.x + 1, self.y):
             moves.append(actions.MoveSouth())
         return moves
- 
+
     def available_actions(self):
         """Returns all of the available actions in this room."""
         moves = []
@@ -41,10 +44,8 @@ class MapTile:
                     moves.append(actions.EquipDagger())
                     break
         return moves
-    
-        
-        
-    
+
+
 class StartingRoom(MapTile):
     """The tile that the player first spawns into."""
     def intro_text(self):
@@ -52,9 +53,8 @@ class StartingRoom(MapTile):
         You find yourself if a cave with a flickering torch on the wall.
         You can make out four paths, each equally as dark and foreboding.
         """
- 
+
     def modify_player(self, player):
-        #Room has no action on player
         pass
 
     def available_actions(self):
@@ -62,7 +62,7 @@ class StartingRoom(MapTile):
         moves = []
         moves = self.adjacent_moves()
         moves.append(actions.ViewInventory())
-        if config.player.easy_mode == True:
+        if config.player.easy_mode is True:
             moves.append(actions.TurnOffEasyMode())
         for a in config.player.inventory:
             if isinstance(a, items.Dagger):
@@ -75,29 +75,31 @@ class StartingRoom(MapTile):
         return moves
 
 
-class EntranceTile(MapTile):
-    """A tile that acts as the introduction to the entire room."""
-    def intro_text(self):
-        return """You are in a large room that is empty save for a terminal to the north."""
-    def modify_player(self, player):
-        player.room_text = self.intro_text()
-    
+# class EntranceTile(MapTile):
+#    """A tile that acts as the introduction to the entire room."""
+#    def intro_text(self):
+#        txt = ("You are in a large room that is empty save for a "
+#               "terminal to the north.")
+#        return txt
+#    def modify_player(self, player):
+#        player.room_text = self.intro_text()
+
 class Door(MapTile):
-    """Doors will always face south to north. Anything north of the door is blocked until the door is unlocked"""
+    """Doors will always face south to north. Anything north
+    of the door is blocked until the door is unlocked"""
     def __init__(self, x, y):
         super().__init__(x, y)
-    
+
     def intro_text(self):
         for a in config.player.inventory:
             if isinstance(a, items.Key):
                 return """The door is unlocked. You may walk through."""
                 break
         return """The door is locked. Find a key to unlock it."""
-        
-                
+
     def modify_player(self, player):
         pass
-    
+
     def adjacent_moves(self):
         """Returns all move actions for adjacent tiles."""
         moves = []
@@ -112,12 +114,10 @@ class Door(MapTile):
         if world.tile_exists(self.x + 1, self.y):
             moves.append(actions.MoveSouth())
         return moves
-    
-    
+
     def available_actions(self):
         """Returns all of the available actions in this room."""
         moves = []
-        #try recreating adjacent moves instead of removing a single adjacent move
         moves = self.adjacent_moves()
         moves.append(actions.ViewInventory())
         for a in config.player.inventory:
@@ -129,14 +129,14 @@ class Door(MapTile):
                     moves.append(actions.EquipDagger())
                     break
         return moves
-    
-    
+
+
 class LootRoom(MapTile):
     """Template for all tiles with items"""
     def __init__(self, x, y, item):
         self.item = [item]
         super().__init__(x, y)
- 
+
     def available_actions(self):
         moves = []
         moves = self.adjacent_moves()
@@ -155,9 +155,9 @@ class LootRoom(MapTile):
             print(item_string)
             config.player.grab_string.append(item_string)
             moves.append(actions.GrabItem(item=self.item))
-            
+
         return moves
-            
+
     def modify_player(self, player):
         pass
 
@@ -167,11 +167,10 @@ class EnemyRoom(MapTile):
     def __init__(self, x, y, enemy):
         self.enemy = enemy
         super().__init__(x, y)
- 
+
     def modify_player(self, the_player):
         if self.enemy.is_alive():
             the_player.hp = the_player.hp - self.enemy.damage
-            #make this loop repeat infinitely
             random_attack = random.randrange(len(self.enemy.attacks))
             print(self.enemy.attacks[random_attack])
 
@@ -182,7 +181,7 @@ class EnemyRoom(MapTile):
                 for b in self.enemy.weaknesses.values():
                     string = "{} {}".format(a, b)
                     config.player.available_attacks.append(string)
-                    if config.player.easy_mode == True:
+                    if config.player.easy_mode is True:
                         print(string)
                     moves = [actions.Attack(enemy=self.enemy)]
             for a in config.player.inventory:
@@ -206,18 +205,18 @@ class EnemyRoom(MapTile):
                         moves.append(actions.EquipDagger())
                         break
             return moves
-    
-        
-        
+
+
 class EmptyCavePath(MapTile):
     def intro_text(self):
         return """
         Another unremarkable part of the cave. You must forge onwards.
         """
- 
+
     def modify_player(self, player):
-        #Room has no action on player
+        # Room has no action on player
         pass
+
 
 class RightCorner(MapTile):
     """Same as EmptyCavePath, but different flavor text."""
@@ -227,8 +226,9 @@ class RightCorner(MapTile):
         """
 
     def modify_player(self, player):
-        #Room has no action on player
+        # Room has no action on player
         pass
+
 
 class LeftCorner(MapTile):
     """Same as EmptyCavePath, but different flavor text."""
@@ -238,31 +238,38 @@ class LeftCorner(MapTile):
         """
 
     def modify_player(self, player):
-        #Room has no action on player
+        # Room has no action on player
         pass
+
 
 class HealingFountain(MapTile):
     """Heals the player to full HP."""
     def intro_text(self):
-        return """
-        You see a glowing fountain before you. The water looks so cool and refreshing that you are filled with determination for whatever lies ahead. You dunk your entire face in and drink as much as you can. Full HP!
-        """
+        txt = ("You see a glowing fountain before you. The water "
+               "looks so cool and refreshing that you are filled "
+               "with determination for whatever lies ahead. You "
+               "dunk your entire face in and drink as much as you "
+               "can. Full HP!")
+        return txt
+
     def modify_player(self, player):
         config.player.hp = 16
+
 
 class GiantSpiderRoom(EnemyRoom):
     def __init__(self, x, y):
         super().__init__(x, y, enemies.GiantSpider())
- 
+
     def intro_text(self):
         if self.enemy.is_alive():
-            return """
-            A giant spider jumps down from its web in front of you! It has huge red eyes and long hairy legs.
-            """
+            txt = ("A giant spider jumps down from its web in front "
+                   "of you! It has huge red eyes and long hairy legs.")
+            return txt
         else:
             return """
             The corpse of a dead spider rots on the ground.
             """
+
 
 class OgreRoom(EnemyRoom):
     def __init__(self, x, y):
@@ -270,26 +277,30 @@ class OgreRoom(EnemyRoom):
 
     def intro_text(self):
         if self.enemy.is_alive():
-            return """
-            A vicious, giant ogre wobbles towards you with a huge belly and long dangly arms.
-            """
+            txt = ("A vicious, giant ogre wobbles towards you with a "
+                   "huge belly and long dangly arms.")
+            return txt
         else:
             return """
             You see the remains from the intense skirmish with the ogre.
             """
+
+
 class ImpRoom(EnemyRoom):
     def __init__(self, x, y):
         super().__init__(x, y, enemies.Imp())
 
     def intro_text(self):
         if self.enemy.is_alive():
-            return """
-            You hear a deafening screech above you just as a vicious looking, fiery imp flies towards you. It has large, thin wings and a red, mottled head.
-            """
+            txt = ("You hear a deafening screech above you just as a "
+                   "vicious looking, fiery imp flies towards you. It "
+                   "has large, thin wings and a red, mottled head.")
+            return txt
         else:
             return """
             You see the scorch marks from your battle with the imp.
             """
+
 
 class DraugrRoom(EnemyRoom):
     def __init__(self, x, y):
@@ -297,57 +308,78 @@ class DraugrRoom(EnemyRoom):
 
     def intro_text(self):
         if self.enemy.is_alive():
-            return """
-            As you make your way through the cavern, you hear what sounds like chimes. Suddenly, a rotten Draugr crawls out of a nearby coffin, equipped with a huge broadsword. It is wearing rusted armor, with an exposed ribcage and pelvis. He immediately sees you and attacks.
-            """
+            txt = ("As you make your way through the cavern, you hear "
+                   "what sounds like chimes. Suddenly, a rotten Draugr "
+                   "crawls out of a nearby coffin, equipped with a huge "
+                   "broadsword. It is wearing rusted armor, with an "
+                   "exposed ribcage and pelvis. He immediately sees you "
+                   "and attacks.")
+            return txt
         else:
             return """
             You see a pile of bones.
             """
- 
+
+
 class FinalBoss(EnemyRoom):
     def __init__(self, x, y):
         super().__init__(x, y, enemies.TieflingKing())
 
     def intro_text(self):
         if self.enemy.is_alive():
-            return """
-            You hear dramatic boss music begin to play and you immediately realize what is about to happen. You suddenly see a pair of bright, burning eyes open in the darkness. The Tiefling King emerges from the other side of the room. He has a long, sharp tail, a powerful jaw with rows of sharp teeth, and sharp claws. You notice it's legs exposed though. The Tiefling King lunges towards you to battle!
-            """
+            txt = ("You hear dramatic boss music begin to play and you "
+                   "immediately realize what is about to happen. You "
+                   "suddenly see a pair of bright, burning eyes open in "
+                   "the darkness. The Tiefling King emerges from the "
+                   "other side of the room. He has a long, sharp tail, a "
+                   "powerful jaw with rows of sharp teeth, and sharp "
+                   "claws. You notice it's legs exposed though. The "
+                   "Tiefling King lunges towards you to battle!")
+            return txt
         else:
             return """
             You see the entrance ahead of you. Victory is within your grasp!
-            """ 
+            """
+
+
 class FindDaggerRoom(LootRoom):
     def __init__(self, x, y):
         super().__init__(x, y, items.Dagger())
-        
+
     def intro_text(self):
         if len(self.item) >= 1:
-            return """\nYou notice a large rug in the center of the room. You lift up the rug to find a sharp dwarven dagger underneath it."""
+            txt = ("\nYou notice a large rug in the center of the room. "
+                   "You lift up the rug to find a sharp dwarven dagger "
+                   "underneath it.")
+            return txt
         else:
             return """
             An unremarkable part of the cave. You must forge onwards.
             """
-        
+
+
 class FindKeyTile(LootRoom):
     def __init__(self, x, y):
         super().__init__(x, y, items.Key())
+
     def intro_text(self):
-        if len(self.item) >=1:
+        if len(self.item) >= 1:
             return """You notice a golden key on a pedestal."""
         else:
-            return """You see a wooden pedestal where the golden key once was."""
-    
+            txt = ("You see a wooden pedestal where the golden key "
+                   "once was.")
+            return txt
+
+
 class LeaveCaveRoom(MapTile):
     """The player must move to this tile to win the game"""
     def intro_text(self):
         return """
         You see a bright light in the distance...
         ... it grows as you get closer! It's sunlight!
-         
+
         Victory is yours!
         """
- 
+
     def modify_player(self, player):
         player.victory = True
